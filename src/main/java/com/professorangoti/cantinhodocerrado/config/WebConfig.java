6d8a53637c4b9d1a09ecb80894b62eb117505bde
angoti <angoti@gmail.com>
2024-10-31 07:13:56 -0300

    cors

---
+++ src/main/java/com/professorangoti/cantinhodocerrado/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.professorangoti.cantinhodocerrado.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("https://parque-app-web-5e794eba0389.herokuapp.com")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
+}
